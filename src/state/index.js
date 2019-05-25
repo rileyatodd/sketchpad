e1830a17e560b6b96ea558191a959cb70915ce2d
Riley Todd <rileyatodd@gmail.com>
2019-05-25 11:54:08 -0500

    Hack to fix state synchronization issue
    Need to figure out a way to do something like
    
    ```
    atom.observe(() => atom.set())
    ```
    
    and have the atom emit in the correct order

--- src/state/index.js
+++ src/state/index.js
@@ -7,14 +7,16 @@ import Boid from '../sketches/boids/Boid'
 export const atom = U.atom({numBoids: 35, boids: [], width: 600, height: 400, paused: false})
 
 atom.view('numBoids').observe(numBoids => {
-   atom.view('boids').modify(boids => {
+   // If we just do modify directly here the atom will emit in the wrong order =\
+   // This setTimeout hack fixes it in a way but there's probably a more idiomatic way to do it
+   setTimeout(() => atom.view('boids').modify(boids => {
      let diff = numBoids - boids.length;
      if (diff === 0) return boids
      if (diff > 0) return boids.concat(R.map(() => new Boid(Math.random() * 600, 
                                                             Math.random() * 400), 
                                              R.range(0, diff)))
      if (diff < 0) return boids.slice(0, numBoids)
-   })
+   }))
 })
 
 Object.assign(window, {U, K, R, L, atom})
